d8786f18d281900f59d4137ed7e5000cab680928
WSmadushanka <wijamu.maduu22@gmail.com>
2025-09-18 16:25:51 +0530

    change code.

--- src/main/java/com/example/nexttravelbackend/service/BookingService.java
+++ src/main/java/com/example/nexttravelbackend/service/BookingService.java
@@ -1,4 +1,4 @@
-package com.example.nexttravelbackend.service;
+'package com.example.nexttravelbackend.service;
 
 import com.example.nexttravelbackend.dto.BookingDTO;
 
@@ -18,3 +18,4 @@ public interface BookingService {
 
     List<Map<String, Object>> getTotalPricePerBooking();
 }
+'
